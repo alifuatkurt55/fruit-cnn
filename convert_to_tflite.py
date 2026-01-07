2295e25d7d33c2cce876abcfe23ec44d98a60e8a
alifuatkurt55 <alifuatkurt.555@gmail.com>
2026-01-07 23:51:13 +0300

    update
    update

---
+++ convert_to_tflite.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+# Mevcut modeli yükle
+model = tf.keras.models.load_model("models/cnn_fruit_best_model.h5")
+
+# TFLite dönüştürücü oluştur
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+
+# Optimizasyonları aç (Boyutu ve RAM'i daha da düşürür)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+# Dönüştür
+tflite_model = converter.convert()
+
+# Kaydet
+with open("models/model.tflite", "wb") as f:
+    f.write(tflite_model)
+
+print("✅ Başarılı! 'models/model.tflite' dosyası oluşturuldu.")
